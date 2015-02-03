5a34d779bf1d102feea3c01cd57724ef186f6e85
mars.tsang <zmars@me.com>
2015-02-03 15:55:27 +0800

    fd

--- iosTest/AppDelegate.m
+++ iosTest/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSString *str = nil;
     return YES;
 }
 
