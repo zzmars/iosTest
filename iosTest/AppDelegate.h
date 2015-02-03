800965800f605b28b7255814c3c90fe54287d8a2
mars.tsang <zmars@me.com>
2015-02-03 15:29:18 +0800

    first init

---
+++ iosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosTest
+//
+//  Created by mars.tsang on 15/2/3.
+//  Copyright (c) 2015年 ___MISHI___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosTest
+//
+//  Created by mars.tsang on 15/2/3.
+//  Copyright (c) 2015年 ___MISHI___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
